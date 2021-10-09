5da84e01897096b2dc6af860308082ffb8d5f54b
RoNSipinoa <whsoh@naver.com>
2021-10-09 23:27:10 +0900

    Add Description...
    Add Description
    Add new consideration to conlang-factors

--- src/main.hs
+++ src/main.hs
@@ -1,37 +1,27 @@
 -- The Noun type refers to nouns, pronouns, and numerals.
--- The Verb type refers to verbs and predicative adjectives.
--- The Mod type, abbreviation for the Modifier type, refers to attributive adjectives, adverbs, and even the qualities nouns and verbs have.
--- For example, one Mod can be used to pluralize nouns, while another Mod can be used to mark perfect tenses on verbs.
+-- The result of conversion from clause to Noun can also be Noun.
 
-data Noun = Noun String | NConvert Phrase deriving (Show)
-
-data Verb = Verb String | VConvert Phrase deriving (Show)
-
-data Mod = Mod String | MConvert Phrase deriving (Show)
-
--- The Nounly typeclass includes anything that can act like nouns.
--- Examples include nouns, noun phrases, pronoun, and numerals.
--- The result of conversion from clause to noun phrase can also be the member of Nounly typeclass.
--- The Verbly typeclass includes anything that can act like verbs.
--- Examples include verbs, verb phrases (often with objects), or so-called "adjectives".
+-- The Verb type refers to verbs and so-called "adjectives".
 -- Note that the adjective here is not that in English, but rather that in Korean. Think "be happy" or "be angry" acting like a verb.
 -- I will refer them to as "predicative adjectives", and the others to as "attributive adjectives".
--- The result of conversion from clause to verb phrase can also be the member of Verbly typeclass. It is usually used in causative sentences.
--- The Modly typeclass includes anything that can act like modifiers.
--- Examples include attributive adjectives, adverbs, and even the qualities nouns and verbs have, like tenses and numbers.
--- The result of conversion from clause to modifier phrase can also be the member of Modly typeclass.
+-- The result of conversion from clause to Verb can also be Verb.
 
-class Nounly a
+-- The Mod type, abbreviation for the Modifier type, refers to attributive adjectives, adverbs, and even the qualities nouns and verbs have.
+-- For example, one Mod can be used to pluralize nouns, while another Mod can be used to mark perfect tenses on verbs.
+-- The result of conversion from clause to Mod can also be Mod.
 
-class Verbly a
+data Noun = Noun String | NConvert String Phrase deriving (Show)
 
-class Modly a
+data Verb = Verb String | VConvert String Phrase deriving (Show)
 
-instance Nounly Noun
+data Mod = Mod String | MConvert String Phrase deriving (Show)
 
-instance Verbly Verb
-
-instance Modly Mod
+-- The Phrase type refers to the structural characteristic of words.
+-- The Noun, Verb, and Mod types above are parts of speech, or POSs. The Phrase type takes those POSs into the syntactic area of sentence.
+-- NWord, VWord, and MWord can be seem as a "converter" from POS to the 'leaf' of syntax tree. 
+-- Add is the 'branch' of syntax tree, and mostly seem as applying the second phrase to the first phrase.
+-- For example, the sentence can be created by adding (or applying) a phrase with the Noun leaf at its head to phrase with the Verb leaf at its head, i.e. Sentence = VP + NP.
+-- In this language, the type of phrase is determined by the first leaf it has, because the language is very strictly head-initial.
 
 data Phrase
   = NWord Noun
