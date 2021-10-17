72fd2e14fa8d8e8036b5dde5a91874076cb04407
RoNSipinoa <whsoh@naver.com>
2021-10-17 10:18:37 +0900

    Update base.hs
    Add Join (Sentence Creation)
    Create Aliases

--- src/Word/base.hs
+++ src/Word/base.hs
@@ -22,17 +22,34 @@ data Mod = Mod String | MConvert String Phrase deriving (Show)
 The Noun, Verb, and Mod types above are parts of speech, or POSs. The Phrase type takes those POSs into the syntactic area of sentence.
 NWord, VWord, and MWord can be seem as a "converter" from POS to the 'leaf' of syntax tree.
 Add is the 'branch' of syntax tree, and mostly seem as applying the second phrase to the first phrase.
-For example, the sentence can be created by adding (or applying) a phrase with the Noun leaf at its head to phrase with the Verb leaf at its head, i.e. Sentence = VP + NP.
-In this language, the type of phrase is determined by the first leaf it has, because the language is very strictly head-initial. -}
+In this language, the type of phrase is determined by the first leaf it has, because the language is very strictly head-initial.
+Join, in the other way, is the way of constructing the sentence from the noun phrase and verb phrase.
+It is not possible to catch the error if the first argument of Join is not verb phrase or the second of that is not noun phrase, as this is just the base for parser.
+However, implementing Join is important, because sentences with intransitive verbs and subjectives should be differentiated from verb phrases with transitive verbs and objectives. -}
 
 data Phrase
   = NWord Noun
   | VWord Verb
   | MWord Mod
   | Add Phrase Phrase
+  | Join Phrase Phrase
   deriving (Show)
 
-infixr 5 +-+
+-- Aliases
 
-(+-+) :: Phrase -> Phrase -> Phrase
-(+-+) = Add
+infixr 5 +!+
+infixr 4 +++
+
+(+!+) :: Phrase -> Phrase -> Phrase
+(+!+) = Add
+
+(+++) :: Phrase -> Phrase -> Phrase
+(+++) = Join
+
+noun = NWord . Noun
+verb = VWord . Verb
+mod = MWord . Mod
+
+nconv x = NWord . NConvert x
+vconv x = VWord . VConvert x
+mconv x = MWord . MConvert x
