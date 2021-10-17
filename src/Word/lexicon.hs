59c7c5f50d9c28b5614b034db5650884b9f91dc2
RoNSipinoa <whsoh@naver.com>
2021-10-17 10:18:00 +0900

    Create lexicon.hs
    Lexicon = Word dictionary. This is the test version and can be changed.

---
+++ src/Word/lexicon.hs
@@ -0,0 +1,10 @@
+module Word.Lexicon where
+
+import Word.Base
+
+gzvu = verb "like"
+zkjn = noun "Python"
+fyeg = noun "Andrew" 
+gawy = verb "think"
+bott = noun "I"
+zqyq = NWord . NConvert "that"
