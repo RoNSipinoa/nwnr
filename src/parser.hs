c6158477b908149c3f8a06f053ce2c58e60c3ab7
RoNSipinoa <whsoh@naver.com>
2021-12-26 15:29:33 +0900

    Update parser.hs
    the language structure followed that of Forth using stack

--- src/parser.hs
+++ src/parser.hs
@@ -1,4 +1,4 @@
-module Word.Parser where
+module Parser where
 
 import Text.Printf ( printf )
 import Prelude hiding ( (<*), (*>) )
@@ -6,6 +6,8 @@ import Base
 import Lexicon
 
 data Tree a = Branch String (Tree a) (Tree a) | Leaf a
+
+data StackElem = StackPhrase Phrase | StackOp (Phrase -> Phrase -> Phrase) 
 
 instance (Show a) => Show (Tree a) where
   show = showAtLevel 0
@@ -23,12 +25,29 @@ phraseType (Join x y) = "Sentence"
 
 
 parse :: Phrase -> Tree Phrase
-parse (NWord (NConvert x y)) = Branch "NConvert" (Leaf . NWord $ Noun x) (parse y)
 parse (NWord x) = Leaf (NWord x)
-parse (VWord (VConvert x y)) = Branch "VConvert" (Leaf . VWord $ Verb x) (parse y)
 parse (VWord x) = Leaf (VWord x)
-parse (MWord (MConvert x y)) = Branch "MConvert" (Leaf . MWord $ Modf x) (parse y)
 parse (MWord x) = Leaf (MWord x)
 parse (Add x y) = Branch (phraseType x) (parse x) (parse y)
 parse (Join x y) = Branch "Sentence" (parse x) (parse y)
 
+isPhrase :: StackElem -> Bool
+isPhrase (StackPhrase _) = True
+isPhrase _ = False
+
+toPhrase :: StackElem -> Phrase
+toPhrase (StackPhrase x) = x
+
+isOp :: StackElem -> Bool
+isOp (StackOp _) = True
+isOp _ = False
+
+toOp :: StackElem -> (Phrase -> Phrase -> Phrase)
+toOp (StackOp a) = a
+
+stack2Phrase :: [StackElem] -> Maybe Phrase
+stack2Phrase xs = let result = foldl stackProcess [] xs in if length result /= 1 then Nothing else Just $ head result
+  where stackProcess stack x
+          | kpnc `elem` stack = [kpnc, kpnc]
+          | isPhrase x    = toPhrase x:stack
+          | isOp x        = if length stack >= 2 then let b:a:xs = stack in (toOp x) a b:xs else [kpnc, kpnc]  -- if there is only one kpnc, the output will be Just kpnc, which is not a desirable result
